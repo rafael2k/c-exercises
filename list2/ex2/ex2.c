d5461de94e26b62bdf66c341cdcc28ecd237bb2c
Rafael Diniz <rafael@riseup.net>
2024-07-18 17:38:13 +0100

    added first exercise

---
+++ list2/ex2/ex2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    int numero_de_elementos = 10;
+    int vetor1[numero_de_elementos];
+    int vetor2[numero_de_elementos];
+
+    // lendo o promeiro vetor
+    for (i = 0; i < numero_de_elementos; i++)
+    {
+        scanf("%d", &vetor1[i]);
+    }
+
+
+    for (i = 0; i < numero_de_elementos; i++)
+    {
+        scanf("%d", &vetor2[i]);
+    }
+
+    for (i = 0; i < numero_de_elementos; i++)
+    {
+        printf("%d %d ", vetor1[i], vetor2[i]);
+    }
+
+    return 0;
+}
